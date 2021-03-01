c4d6e53593a8dda00006b755c3243595340169ad
Glenn Meuth <vortarian@gmail.com>
2021-02-28 23:54:10 -0700

    Updating build script to use default values that match .gitlab ci

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+: ${CC:=/opt/llvm-11.1.0/bin/clang}
+: ${CXX:=/opt/llvm-11.1.0/bin/clang++}
+: ${CXXFLAGS:="-std=c++20 -stdlib=libc++"}
+: ${CMAKE_BUILD_TYPE:="Release"}
+
 ROOT_DIR=$(dirname $0)
 mkdir -p ${ROOT_DIR}/build
 pushd ${ROOT_DIR}/build
-cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 make
 popd # ${ROOT_DIR}/build

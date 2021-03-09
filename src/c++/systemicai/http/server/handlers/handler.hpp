5f623b152b4c8b8836cc47778ec9844fbf18a91c
Glenn Meuth <glenn.meuth@systemicai.com>
2021-03-08 23:13:07 -0700

    Updated handler to allow registry of global handlers;  Unit tests aren't setup for this yet;

--- src/c++/systemicai/http/server/handlers/handler.hpp
+++ src/c++/systemicai/http/server/handlers/handler.hpp
@@ -11,10 +11,81 @@ namespace systemicai::http::server::handlers {
 
 template <class Body, class Allocator, class Send> class handler {
 public:
-  int handle(boost::beast::http::request< Body, boost::beast::http::basic_fields<Allocator> > &req) {
+  int handle(boost::beast::http::request< Body, boost::beast::http::basic_fields<Allocator> > &req, Send& send) {
     return 200;
   };
 };
+
+// Http Request as a templated alias
+template <class Body, class Allocator>
+using Request = beast::http::request<Body, beast::http::basic_fields<Allocator>>;
+
+// Handler Function as a templated alias
+// TODO: Not using move sematics here because the functions will execute in a loop
+// Determine if we should switch this to move semantics and add in a handles(...) type of method
+template< class Body, class Allocator, class Send> 
+using HandlerFunction = bool(*)(Request<Body, Allocator>& req, Send& send, const settings&);
+
+// Collection for the handlers
+template< class Body, class Allocator, class Send>
+using HandlerCollection = std::list<HandlerFunction<Body, Allocator, Send> >;
+
+// Setup a registry for _compile time_ addition of handlers for the service
+template< class Body, class Allocator, class Send>
+class HandlerRegistry {
+public:
+  HandlerRegistry() {
+  }
+
+  HandlerRegistry(const HandlerRegistry& hr) {
+    std::copy(hr.begin(), hr.end(), this->handlers_.begin());
+  }
+
+  HandlerRegistry(HandlerRegistry&& hr) : handlers_(std::move(hr.handlers_)) {
+  }
+
+  void addHandler(HandlerFunction<Body, Allocator, Send>* h) {
+      handlers_.insert(handlers_.end(), h);
+  }
+
+  const HandlerCollection<Body, Allocator, Send> handlers() const {
+      return handlers_;
+  }
+private:
+  HandlerCollection<Body, Allocator, Send> handlers_;
+};
+
+template< class Body, class Allocator, class Send>
+class GlobalHandlerRegistry : public HandlerRegistry< Body, Allocator, Send>
+{
+public:
+  /**
+   * Provide access to a global registry
+   */
+  static GlobalHandlerRegistry& global() {
+     static GlobalHandlerRegistry<Body, Allocator, Send> registry;
+     return registry;
+  }
+
+  /**
+   * Provide a means of getting a copy of the handlers since we are blocking all use of constructors & operators
+   */
+  HandlerRegistry<Body, Allocator, Send>&& toHandlerRegistry() {
+    HandlerRegistry<Body, Allocator, Send> hr;
+    std::copy(this->begin(), this->end(), hr.handlers_.begin());
+    return hr;
+  }
+
+private:
+  GlobalHandlerRegistry() { ; }
+
+  // Prevent Move Semantics and construction
+  GlobalHandlerRegistry(GlobalHandlerRegistry&&) = delete;
+  GlobalHandlerRegistry(GlobalHandlerRegistry&) = delete;
+  GlobalHandlerRegistry& operator=(const GlobalHandlerRegistry<Body, Allocator, Send>&) = delete;
+};
+
+static HandlerRegistry< class Body, class Allocator, class Send> Registry;
 
 // This function produces an HTTP response for the given
 // request. The type of the response object depends on the
@@ -24,7 +95,7 @@ template<
         class Body, class Allocator,
         class Send>
 void
-handle_request(
+default_handle_request(
         beast::string_view doc_root,
         beast::http::request<Body, beast::http::basic_fields<Allocator>>&& req,
         Send&& send,
@@ -122,6 +193,33 @@ handle_request(
     res.content_length(size);
     res.keep_alive(req.keep_alive());
     return send(std::move(res));
+}
+
+// This handler allows override of the default handlers by overriding the assigned handler.
+// If no handlers are registered with the GlobalHandlerRegistry then it will call default_handle_request
+template<
+        class Body, class Allocator,
+        class Send>
+void
+handle_request(
+        beast::string_view doc_root,
+        beast::http::request<Body, beast::http::basic_fields<Allocator>>&& req,
+        Send&& send,
+        const settings& s)
+{
+  auto gHandlers = GlobalHandlerRegistry<Body, Allocator, Send>::global().handlers();
+  if(gHandlers.empty()) {
+    bool handled = false;
+    for(auto handler : gHandlers) {
+        handled = handler(req, send, s);
+        if(handled) break;
+    }
+    if(!handled) {
+      default_handle_request(s.document_root, std::move(req), std::move(send), s);
+    }
+  } else {
+      default_handle_request(s.document_root, std::move(req), std::move(send), s);
+  }
 }
 
 } // namespace systemicai::http::server::handler
